3edb278052cd64f3d46d39ad50d6d6dbeb21c208
nyomsas99 <nyomsas1234@gmail.com>
2023-01-23 18:17:35 +0700

    menambahkan button sosial media

--- js/index.js
+++ js/index.js
@@ -5,3 +5,28 @@ chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
   title.classList.toggle("jumdark");
 });
+
+// my social media
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
+});
+
+const abt = document.querySelector(".abt");
+
+abt.addEventListener("mouseenter", () => {
+  abt.style.backgroundColor = "rgb(100, 50, 120)";
+});
+
+abt.addEventListener("mouseleave", () => {
+  abt.style.backgroundColor = "red";
+});
